cce25af57fe18424b11a553d4bb154d439a0931b
eduardobattisti <eduardobattisti10@gmail.com>
2022-05-01 18:12:09 -0300

    feat: use readme template and remove unnecessary files

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import AutoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     AutoImport({
-      imports: 'react',
+      imports: "react",
     }),
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
   },
 });
