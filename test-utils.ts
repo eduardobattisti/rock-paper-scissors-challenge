e43a2152cf3fb8e2ce539558be8f3a89879de2f7
eduardobattisti <eduardobattisti10@gmail.com>
2022-05-08 19:40:57 -0300

    feat: create a test utils

--- test-utils.ts
+++ test-utils.ts
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render } from "@testing-library/react";
 
 const customRender = (ui: JSX.Element, options = {}) =>
   render(ui, {
@@ -7,8 +7,8 @@ const customRender = (ui: JSX.Element, options = {}) =>
     ...options,
   });
 
-export * from '@testing-library/react';
-export { default as userEvent } from '@testing-library/user-event';
+export * from "@testing-library/react";
+export { default as userEvent } from "@testing-library/user-event";
 
 // override render export
 export { customRender as render };
