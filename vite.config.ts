48d1f3ab3843f7116984eba0f36b8ff4b14e2ebe
eduardobattisti <eduardobattisti10@gmail.com>
2022-05-08 19:39:03 -0300

    feat: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import AutoImport from "unplugin-auto-import/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +16,13 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./setupTests.ts",
+    silent: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
+    },
   },
 });
