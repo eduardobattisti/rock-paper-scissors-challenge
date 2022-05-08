2999967dabb445b592e63b60db7495094a5e1788
eduardobattisti <eduardobattisti10@gmail.com>
2022-05-08 19:42:19 -0300

    feat: create styled type

---
+++ src/styled.d.ts
@@ -0,0 +1,60 @@
+// import original module declarations
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface Theme {
+    font: {
+      weight: {
+        semiBold: number;
+        bold: number;
+      };
+      size: {
+        tiny: string;
+        base: string;
+        lg: string;
+      };
+    };
+    colors: {
+      white: string;
+      primary: {
+        scissors: {
+          from: string;
+          to: string;
+        };
+        paper: {
+          from: string;
+          to: string;
+        };
+        rock: {
+          from: string;
+          to: string;
+        };
+        lizard: {
+          from: string;
+          to: string;
+        };
+        cyan: {
+          from: string;
+          to: string;
+        };
+      };
+      neutral: {
+        darkText: string;
+        scoreText: string;
+        headerOutline: string;
+      };
+      background: {
+        from: string;
+        to: string;
+      };
+    };
+    screens: {
+      mobile: string;
+      tablet: string;
+      desktop: string;
+      hd: string;
+      full: string;
+    };
+  }
+}
