699344be6ba3c3dd2d776368d97a9c34730f82a8
eduardobattisti <eduardobattisti10@gmail.com>
2022-05-08 19:44:02 -0300

    feat: create and define a default export on Title component

--- src/components/Title/Title.component.tsx
+++ src/components/Title/Title.component.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const Title = () => {
-  return <h1>Hello World</h1>;
+import styled from "styled-components";
+import { ITitleProps } from "./Title.interface";
+
+const HeadingStyle = styled.h1<Omit<ITitleProps, "tag">>`
+  font-size: ${({ theme, fs }) => theme.font.size[fs]};
+  color: ${({ theme, color }) => theme.colors[color]};
+  font-weight: ${({ theme, fw }) => theme.font.weight[fw]};
+`;
+
+const Title = ({ tag = "h1", fs, fw, color, children }: ITitleProps) => {
+  const allowedTags = ["h1", "h2", "h3", "h4", "h5", "h6"];
+
+  return (
+    <HeadingStyle
+      as={allowedTags.includes(tag) ? tag : "h1"}
+      fs={fs}
+      fw={fw}
+      color={color}
+    >
+      {children}
+    </HeadingStyle>
+  );
 };
 
-export default Title;
+export { Title };
