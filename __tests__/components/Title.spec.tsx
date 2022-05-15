48e1bd29b1ff7271c9c1a2d28fccca834300e17f
eduardobattisti <eduardobattisti10@gmail.com>
2022-05-15 17:27:38 -0300

    fix: add correct methods to wrap the component

--- __tests__/components/Title.spec.tsx
+++ __tests__/components/Title.spec.tsx
@@ -1,52 +1,42 @@
 import React from "react";
-import { unmountComponentAtNode } from "react-dom";
 import Title from "@components/Title";
 import { HeadingTag } from "@components/Title/Title.interface";
 import Theme from "@/Theme";
-import { render } from "../../test-utils";
+import { render, cleanup } from "@testing-library/react";
 
 describe("Title Component", () => {
   let wrapperComponent: HTMLDivElement;
 
-  const wrapper = (container: HTMLDivElement, tag: HeadingTag = "h2") => {
+  const wrapper = (tag: HeadingTag = "h2") => {
     return render(
       <Theme>
         <Title tag={tag} fs="base" fw="semiBold" color="white">
           Title Test
         </Title>
-      </Theme>,
-      container
+      </Theme>
     );
   };
 
-  beforeEach(() => {
-    wrapperComponent = document.createElement("div");
-    document.body.appendChild(wrapperComponent);
-  });
-
-  afterEach(() => {
-    unmountComponentAtNode(wrapperComponent);
-    wrapperComponent.remove();
-  });
+  afterEach(cleanup);
 
   it("Should render Title without errors", () => {
-    wrapper(wrapperComponent);
+    expect(wrapper()).toBeDefined();
   });
 
   it("Should render the text provided", () => {
-    const { getByText } = wrapper(wrapperComponent);
+    const { getByText } = wrapper();
     const titleText = getByText(/Title Test/i);
-    expect(titleText).toBeInTheDocument();
+    expect(titleText).toBeDefined();
   });
 
   it("Should be mounted with a H2 tag", () => {
-    const { container } = wrapper(wrapperComponent);
+    const { container } = wrapper();
     const baseElement = container.querySelector("h2");
     expect(baseElement?.tagName).toBe("H2");
   });
 
   it("Should render a H1, if an unavailable tag is passed", () => {
-    const { container } = wrapper(wrapperComponent, "a");
+    const { container } = wrapper("a");
     const baseElement = container.querySelector("h1");
     expect(baseElement?.tagName).toBe("H1");
   });
