fde19cef39154379129542f138e297e12f5f1760
eduardobattisti <eduardobattisti10@gmail.com>
2022-05-15 18:35:16 -0300

    refactor: use themeWrapper and refactor tests suites

--- __tests__/components/Title.spec.tsx
+++ __tests__/components/Title.spec.tsx
@@ -1,42 +1,46 @@
 import React from "react";
 import Title from "@components/Title";
-import { HeadingTag } from "@components/Title/Title.interface";
-import Theme from "@/Theme";
-import { render, cleanup } from "@testing-library/react";
+import themeWrapper from "../helpers/themeWrapper";
+import { cleanup } from "@testing-library/react";
 
 describe("Title Component", () => {
-  let wrapperComponent: HTMLDivElement;
-
-  const wrapper = (tag: HeadingTag = "h2") => {
-    return render(
-      <Theme>
-        <Title tag={tag} fs="base" fw="semiBold" color="white">
-          Title Test
-        </Title>
-      </Theme>
-    );
-  };
-
   afterEach(cleanup);
 
   it("Should render Title without errors", () => {
-    expect(wrapper()).toBeDefined();
+    const wrapper = themeWrapper(
+      <Title tag="h2" fs="base" fw="semiBold" color="white">
+        Title Test
+      </Title>
+    );
+    expect(wrapper).toBeDefined();
   });
 
   it("Should render the text provided", () => {
-    const { getByText } = wrapper();
+    const { getByText } = themeWrapper(
+      <Title tag="h2" fs="base" fw="semiBold" color="white">
+        Title Test
+      </Title>
+    );
     const titleText = getByText(/Title Test/i);
     expect(titleText).toBeDefined();
   });
 
   it("Should be mounted with a H2 tag", () => {
-    const { container } = wrapper();
+    const { container } = themeWrapper(
+      <Title tag="h2" fs="base" fw="semiBold" color="white">
+        Title Test
+      </Title>
+    );
     const baseElement = container.querySelector("h2");
     expect(baseElement?.tagName).toBe("H2");
   });
 
   it("Should render a H1, if an unavailable tag is passed", () => {
-    const { container } = wrapper("a");
+    const { container } = themeWrapper(
+      <Title tag="a" fs="base" fw="semiBold" color="white">
+        Title Test
+      </Title>
+    );
     const baseElement = container.querySelector("h1");
     expect(baseElement?.tagName).toBe("H1");
   });
