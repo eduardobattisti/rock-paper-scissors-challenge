d2329c06831d3aa898a52fbae25a5df5a8255e63
eduardobattisti <eduardobattisti10@gmail.com>
2022-06-18 20:15:04 -0300

    feat: add letter spacing property

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -8,12 +8,21 @@ const ButtonStyle = styled.button<IButtonProps>`
     variant === Variants.OUTLINE
       ? theme.colors.white
       : theme.colors.neutral.darkText};
-  font-weight: ${({ theme }) => theme.font.weight.bold};
+  font-weight: ${({ theme }) => theme.font.weight.semiBold};
+  letter-spacing: ${({ theme }) => theme.font.spacing.small};
   background-color: ${({ theme, variant }) =>
-    variant === Variants.OUTLINE
-      ? theme.colors.background.to
-      : theme.colors.white};
+    variant === Variants.OUTLINE ? "transparent" : theme.colors.white};
   border: ${({ theme }) => "1px solid " + theme.colors.white};
+  border-radius: 8px;
+  padding: 0.7rem 2rem;
+
+  &:hover {
+    color: ${({ theme, variant }) =>
+      variant === Variants.OUTLINE
+        ? theme.colors.white
+        : theme.colors.primary.rock.from};
+    cursor: pointer;
+  }
 `;
 
 const Button = ({ variant, children, onClick }: IButtonProps) => {
