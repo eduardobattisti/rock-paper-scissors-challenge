dae9e11c306928a563229f8288a60accf19b1574
eduardobattisti <eduardobattisti10@gmail.com>
2022-06-23 19:49:59 -0300

    feat: define styles for chip

--- src/components/Chip/Chip.style.tsx
+++ src/components/Chip/Chip.style.tsx
@@ -1,23 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import { IChipProps } from "./Chip.interface";
+import { IChipProps, ChipTypes } from "./Chip.interface";
 import RockImg from "@/assets/images/icon-rock.svg";
+import PaperImg from "@/assets/images/icon-paper.svg";
+import ScissorsImg from "@/assets/images/icon-scissors.svg";
+
+const imageByType = (type: string) => {
+  switch (type) {
+    case ChipTypes.PAPER:
+      return PaperImg;
+    case ChipTypes.ROCK:
+      return RockImg;
+    case ChipTypes.SCISSOR:
+      return ScissorsImg;
+  }
+};
 
 const ChipStyle = styled.div<IChipProps>`
-  margin-top: 100px;
-  margin-left: 100px;
-
   width: 255px;
   height: 255px;
   border-radius: 50%;
   cursor: pointer;
-  background-image: url(${RockImg});
+  background-image: url(${({ type }) => imageByType(type)});
   background-position: center center;
   background-repeat: no-repeat;
   background-size: 40%;
   background-color: ${({ theme }) => theme.colors.white};
-  box-shadow: inset 0 20px 0 -5px hsl(0 0% 90%), 0 0 0 35px hsl(349, 70%, 56%),
-    0 12px 0 35px hsl(349 65% 47%);
+  box-shadow: ${({ theme, type }) =>
+    `inset 0 20px 0 -5px hsl(0 0% 90%), 
+    0 0 0 35px ${theme.colors.primary[type].to}, 
+    0 12px 0 35px ${theme.colors.primary[type].from}`};
 `;
 
 export default ChipStyle;
