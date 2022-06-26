ef22a979b27958fd4238416bee2c07625686e3d5
eduardobattisti <eduardobattisti10@gmail.com>
2022-06-26 13:22:52 -0300

    refactor: update chip size and positions

--- src/components/Chip/Chip.style.tsx
+++ src/components/Chip/Chip.style.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { IChipProps, ChipTypes } from "./Chip.interface";
+import { IChipProps, ChipTypes, ChipPositions } from "./Chip.interface";
 import RockImg from "@/assets/images/icon-rock.svg";
 import PaperImg from "@/assets/images/icon-paper.svg";
 import ScissorsImg from "@/assets/images/icon-scissors.svg";
@@ -16,11 +16,23 @@ const imageByType = (type: string) => {
   }
 };
 
+const positionByType = (type: string) => {
+  switch (type) {
+    case ChipTypes.PAPER:
+      return ChipPositions.paper;
+    case ChipTypes.ROCK:
+      return ChipPositions.rock;
+    case ChipTypes.SCISSOR:
+      return ChipPositions.scissors;
+  }
+};
+
 const ChipStyle = styled.div<IChipProps>`
-  width: 255px;
-  height: 255px;
+  width: 220px;
+  height: 220px;
   border-radius: 50%;
   cursor: pointer;
+  margin: auto;
   background-image: url(${({ type }) => imageByType(type)});
   background-position: center center;
   background-repeat: no-repeat;
@@ -30,6 +42,8 @@ const ChipStyle = styled.div<IChipProps>`
     `inset 0 20px 0 -5px hsl(0 0% 90%), 
     0 0 0 35px ${theme.colors.primary[type].to}, 
     0 12px 0 35px ${theme.colors.primary[type].from}`};
+  grid-column: ${({ type }) => positionByType(type)?.column};
+  grid-row: ${({ type }) => positionByType(type)?.row};
 `;
 
 export default ChipStyle;
